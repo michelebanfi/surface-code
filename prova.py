53fcc8fc7aa5c3daf4bb4cae44aab56f214a43f4
michelebanfi <michi.banfi01@gmail.com>
2025-01-31 21:41:37 +0100

    working checkpoint

--- prova.py
+++ prova.py
@@ -1,17 +1,8 @@
-def analyze_surface_code(stabilizer_map, ancilla_data, expected_ancilla='00000000', rounds):
+from pymatching import Matching
 
-
-
-# Example usage
-stabilizer_map = {
-    1: [0, 2, 4],
-    3: [0, 4, 6],
-    5: [8, 2, 4],
-    7: [8, 4, 6]
-}
-
-ancilla_data = {'00000000': 976, '00010000': 25, '00010000': 23}
-rounds = 2
-expected_ancilla = '00000000'
-
-analyze_surface_code(stabilizer_map, ancilla_data, expected_ancilla, rounds)
+grid_size = 3
+n_rounds = 2
+syndrome_data = "01100110"
+matching = Matching.from_grid(grid_size, grid_size, repetitions=n_rounds)
+# Decode syndromes to get correction
+correction = matching.decode(syndrome_data)
