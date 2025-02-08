b19ac55ab2ef8920307a8d48da30cc97f7c4c714
michelebanfi <michi.banfi01@gmail.com>
2025-02-08 12:05:17 +0100

    better logical operators rapresentation

--- plot/logical_operators.py
+++ plot/logical_operators.py
@@ -5,7 +5,7 @@ from matplotlib.patches import Rectangle
 fig, ax = plt.subplots(figsize=(8, 8))
 
 # Define grid size (number of stabilizers in each dimension)
-grid_size = 7
+grid_size = 9
 
 # Generate positions for a chessboard pattern
 positions = [(i, j) for i in range(grid_size) for j in range(grid_size)]
@@ -34,8 +34,7 @@ for i in range(grid_size):
             else:
                 data_qubits.append((i / 2, j / 2))
 
-
-# remove the stabilizer at coordinates (2.0, 3.5)
+# remove the stabilizer at coordinates (2.0, 2.5)
 stabilizer_positions.remove((2.0, 2.5))
 
 # Plot colored "+"-shaped regions for stabilizers
@@ -87,17 +86,26 @@ for (x, y) in logical_hole:
 for (x, y) in data_qubits:
     ax.add_patch(plt.Circle((x, y), radius=0.1, color='white', ec='black', lw=2, zorder=5))
 
-
 # Plot stabilizers (black circles)
 for (i, j) in stabilizer_positions:
     ax.add_patch(plt.Circle((i, j), radius=0.1, color='black', zorder=3))
 
+# Add double arrow with label
+ax.annotate(
+    '', xy=((grid_size / 2) + 0.2, grid_size / 2), xytext=((grid_size / 2) + 0.2, -0.5),
+    arrowprops=dict(arrowstyle='<->', color='black', lw=4)
+)
+ax.text(grid_size / 2 , grid_size / 4 - 0.1, '$d = 5$', color='black', fontsize=17, ha='center', va='center', backgroundcolor='white')
 
 # Adjust axis limits and appearance
-ax.set_xlim(-0.5, grid_size / 2)
-ax.set_ylim(-0.5, grid_size / 2)
+ax.set_xlim(-0.5, (grid_size / 2) + 0.3)
+ax.set_ylim(-0.5, (grid_size / 2) + 0.3)
 
 ax.set_aspect('equal')
 plt.axis('off')
+
+# Add text annotations
+ax.text(-0.7, 1.13, '$\\hat{X}_L$', color='blue', fontsize=30, ha='center', weight="bold")
+ax.text(1.2, 4.7, '$\\hat{Z}_L$', color='red', fontsize=30, ha='center', weight="bold")
 
 plt.savefig("surface_code_plot_logical_operator.png", dpi=300) #bbox_inches="tight"
